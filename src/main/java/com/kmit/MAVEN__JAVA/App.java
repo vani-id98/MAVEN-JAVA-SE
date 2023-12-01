5ffd9d31263671ee23899d79844cf32d7a3fb8dc
vani-id98 <vkonda673@gmail.com>
2023-12-01 21:08:10 +0530

    msg

---
+++ src/main/java/com/kmit/MAVEN__JAVA/App.java
@@ -0,0 +1,13 @@
+package com.kmit.MAVEN__JAVA;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
